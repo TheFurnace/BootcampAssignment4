47ea5b383d5d4a6e025b9123b4c6f0923de26b23
Fernando Quintero <ferdanielq@outlook.com>
2018-09-25 16:37:34 -0400

    some changes

---
+++ server/config/config.js
@@ -0,0 +1,10 @@
+//This file holds any configuration variables we may need
+//'config.js' is ignored by git to protect sensitive information, such as your database's username and password
+//copy this file's contents to another file 'config.js' and store your MongoLab uri there
+
+module.exports = {
+  db: {
+    uri: "mongodb://ferndq:softweng3031@ds161322.mlab.com:61322/ferndq-softweng" //place the URI of your mongo database here.
+  },
+  port: 8080
+};
