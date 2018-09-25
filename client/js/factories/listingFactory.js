d772028752b803355c7c027c423c9b71bcd1102d
Fernando Quintero <ferdanielq@outlook.com>
2018-09-25 17:06:51 -0400

    dfdfsdf

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -2,15 +2,15 @@ angular.module("listings", []).factory("Listings", function($http) {
   var _url = "https://uf-directory-app-ferndq.herokuapp.com";
   var methods = {
     getAll: function() {
-      return $http.get(url + "/api/listings");
+      return $http.get(_url + "/api/listings");
     },
 
     create: function(listing) {
-      return $http.post(url + "/api/listings", listing);
+      return $http.post(_url + "/api/listings", listing);
     },
 
     delete: function(id) {
-      return $http.delete(url + "/api/listings/" + id);
+      return $http.delete(_url + "/api/listings/" + id);
     }
   };
 
